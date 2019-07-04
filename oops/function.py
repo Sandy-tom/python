5aeb78365dccf04c14bb507e766fc04429caabfc
Santhosh <49803785+Sandy-tom@users.noreply.github.com>
2019-07-04 19:31:38 +0530

    Update function.py

--- oops/function.py
+++ oops/function.py
@@ -1,1 +1,22 @@
+ // program 1:
+def oops(a,b): // by passing parameter in method  
+    c=a + b
+    print("sum of two number",c) 
+a = int(input("enter the nuber"))
+b = int(input("enter the nuber"))
+oops(a,b)
 
+
+
+// program 2:
+    def oops(a):
+   if(a%2==0):
+       print("it is even number")
+   else:
+       print("odd numer")
+
+a= int(input("enter the a value"))
+oops(a)
+    
+  //program 3:
+    
