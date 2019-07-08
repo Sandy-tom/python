0e9e23975bc4ee20c2fb91f39429e226e4c268dd
Santhosh <49803785+Sandy-tom@users.noreply.github.com>
2019-07-08 20:15:38 +0530

    Update lambda.py

--- core/lambda.py
+++ core/lambda.py
@@ -1,1 +1,21 @@
 
+//A lambda function is a small anonymous function.
+
+A lambda function can take any number of arguments, but can only have one expression.
+
+
+//program 1:
+  x= lambda a: a+10
+  print(x(10))
+  
+  
+  //program 2:
+    x = lambda a,b: a * b // anonymous function with key word lambda 
+    print(x(3,4))
+    
+    //program 3:
+    x = lambda a,b,c: a+b+c
+    print(x(2,3,4))
+     
+     // program 4:
+      
